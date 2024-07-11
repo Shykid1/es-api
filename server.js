a5d5907f24be4b71f21591e8a6f20714801452d5
Yussif Yahuza <yussifyahuza12@gmail.com>
2024-07-11 01:25:59 +0000

    chore: Remove CORS middleware in server.js
    This commit removes the `app.use(cors())` middleware in the server.js file. By disabling cross-origin resource sharing (CORS) for all routes, this change improves the server configuration.

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require("cors");
+// const cors = require("cors");
 const dbConnect = require("./utils/db");
 
 // Load env variables

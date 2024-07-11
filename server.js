e1aed3aebdb3378c8d22f2c4470410626a3c596e
Yussif Yahuza <yussifyahuza12@gmail.com>
2024-07-11 02:57:33 +0000

    chore: Update CORS options in server.js file
    This commit updates the CORS options in the server.js file to allow requests from http://localhost:3000 as the origin. This change improves the server configuration by specifying a specific origin for CORS requests.

--- server.js
+++ server.js
@@ -16,13 +16,6 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(cors());
-app.use(function (req, res, next) {
-  res.setHeader("Access-Control-Allow-Origin", "http://localhost:3000");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type");
-  res.setHeader("Access-Control-Allow-Credentials", true);
-  next();
-});
 
 // Routes
 app.use("/api", require("./routes/routes"));
